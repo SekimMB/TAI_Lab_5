526772510876f64c007da0fd637200a41d53be5f
Marcin_Borowski <marcin.boro1@interia.eu>
2020-03-29 19:02:59 +0200

    Task 2 done

--- complex.js
+++ complex.js
@@ -1,27 +1,27 @@
-var Complex = /** @class */ (function () {
-    function Complex(rl, img) {
+"use strict";
+class Complex {
+    constructor(rl, img) {
         this.real = rl;
         this.imaginary = img;
     }
-    Complex.prototype.add = function (imNum1) {
+    add(imNum1) {
         return new Complex(imNum1.real + this.real, imNum1.imaginary + this.imaginary);
-    };
-    Complex.prototype.subtract = function (imNum1) {
+    }
+    subtract(imNum1) {
         return new Complex(this.real - imNum1.real, this.imaginary - imNum1.imaginary);
-    };
-    Complex.prototype.mod = function () {
+    }
+    mod() {
         return Math.sqrt(Math.pow(this.real, 2) + Math.pow(this.imaginary, 2));
-    };
-    Complex.prototype.toString = function () {
+    }
+    toString() {
         return this.real + " + " + this.imaginary + "i";
-    };
-    return Complex;
-}());
-var num1 = new Complex(2, 3);
-var num2 = new Complex(5, 6);
-var num3 = num1.add(num2);
+    }
+}
+let num1 = new Complex(2, 3);
+let num2 = new Complex(5, 6);
+let num3 = num1.add(num2);
 console.log("Dodawanie " + num3.toString());
-var num4 = num3.subtract(num1);
+let num4 = num3.subtract(num1);
 console.log("Odejmowanie: " + num4.toString());
-var modulo = num4.mod();
+let modulo = num4.mod();
 console.log("modul: " + modulo);
