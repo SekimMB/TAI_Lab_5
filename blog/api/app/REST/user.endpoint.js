57b3c4d511a153355006cb289628a533946d5fe9
Marcin_Borowski <marcin.boro1@interia.eu>
2020-05-19 18:48:25 +0200

    added auth to router.post

--- blog/api/app/REST/user.endpoint.js
+++ blog/api/app/REST/user.endpoint.js
@@ -18,7 +18,7 @@ const userEndpoint = (router) => {
     }
   });
 
-  router.post('/api/user/create', async (request, response, next) => {
+  router.post('/api/user/create',auth, async (request, response, next) => {
     try {
       let result = await business(request).getUserManager(request).createNewOrUpdate(request.body);
       response.status(200).send(result);
